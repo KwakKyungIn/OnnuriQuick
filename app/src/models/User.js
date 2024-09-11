0f44b1fa5734a8ea799034239061f4a06a098472
KwakKyungIn <b_mule@naver.com>
2024-09-11 21:44:57 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -22,10 +22,15 @@ class User{
     }
 
 
-    signup(){
+    async signup(){
         const client = this.body;
-        const response = UserStorage.save(client);
-        return response;
+        try{
+            const response = await UserStorage.save(client);
+            return response;
+        }catch(err){
+            return {success : false, msg : err};
+          
+        }
         }
 
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -40,27 +40,11 @@ const process = {
         const response = await user.login();
         return res.json(response);
  
-    //     const id = req.body.id,
-    //         password = req.body.password;
-        
-    //     const users = UserStorage.getUsers("id", "password");
-    //     const response ={};
-    //     if(users.id.includes(id)){
-    //         const idx = users.id.indexOf(id);
-    //         if(users.password[idx]===password){
-    //             response.success = true;
-    //             return res.json(response);
-    //         }
-    //     }
-    //     response.success = false;
-    //     response.msg = "로그인에 실패하였습니다";
-
-    //     return res.json(response);
     },
 
-    signup : (req, res) => {
+    signup : async (req, res) => {
         const user = new User(req.body);
-        const response = user.signup();
+        const response = await user.signup();
         return res.json(response);
     },
 
