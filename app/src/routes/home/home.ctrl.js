0a7fac5c6f9084b49b4239ece618cf73dad1ec68
KwakKyungIn <b_mule@naver.com>
2024-09-11 15:02:47 +0900

    홈컨트롤 수정 추가

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -38,7 +38,6 @@ const process = {
     login : (req, res) => {
         const user = new User(req.body);
         const response = user.login();
-        console.log(response);
         return res.json(response);
     //     const id = req.body.id,
     //         password = req.body.password;
